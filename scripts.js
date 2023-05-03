7647793bdecf1920aed7c32119fd680fb52f438a
chemonique <122254404+chemonique@users.noreply.github.com>
2023-05-03 13:57:47 +0200

    changed name, hobbyfunction, parameter of logtwice

---
+++ scripts.js
@@ -0,0 +1,15 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = (parameter) => {
+    console.log(parameter)
+    console.log(parameter)
+  }
+
+function hobbyfunction () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+}
+
+ hobbyfunction()
+ 
