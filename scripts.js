572f85f4e989616d5d4081afb1170042f0e7b50b
chemonique <122254404+chemonique@users.noreply.github.com>
2023-05-03 14:17:55 +0200

    Challenge 2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//challenge 1 
+
 firstName = 'John';
 age = 35;
 hobby = 'Coding';
@@ -13,3 +15,42 @@ function hobbyfunction () {
 
  hobbyfunction()
  
+
+//challenge 2 
+
+const add = (a, b) => { a + b }
+
+const multiply = (a, b) => { a * b }
+
+function internal() {
+	const added = this.add(this.a, this.b)
+	this.multiply(this.a, this.b)
+	return this
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+consol.log(example1.calculate())
+console.log(example2.calculate())
